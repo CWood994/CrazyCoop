2d62a1d47c59e96cce8cede3c1c3de1e3334c046
Benjamin Stammen <benjamin@stammen.me>
2016-10-30 01:59:00 -0400

    make bird class

---
+++ day/BirdSprite.swift
@@ -0,0 +1,21 @@
+//
+//  BirdSprite.swift
+//  day
+//
+//  Created by Benjamin Stammen on 10/30/16.
+//  Copyright © 2016 Benjamin Stammen. All rights reserved.
+//
+
+import SpriteKit
+
+class BirdSprite : SKSpriteNode {
+    
+    override init(texture: SKTexture!, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
